3704694c878ba0639f5f49205b348f83edb04df4
Oleh Andrushko <olich.me@gmail.com>
2023-03-14 10:02:19 +0100

    chore: switch fromn pre-commit to pre-push hook

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-	const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-	const unlockTime = currentTimestampInSeconds + 60;
-
-	const lockedAmount = ethers.utils.parseEther("0.001");
-
-	const Lock = await ethers.getContractFactory("Lock");
-	const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-	await lock.deployed();
-
-	console.log(
-		`Lock with ${ethers.utils.formatEther(
-			lockedAmount,
-		)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`,
-	);
-}
+	// We get the contract to deploy
+	const Greeter = await ethers.getContractFactory("Greeter");
+	const greeter = await Greeter.deploy("Hello, Hardhat!");
+  
+	await greeter.deployed();
+  
+	console.log("Greeter deployed to:", greeter.address);
+  }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
