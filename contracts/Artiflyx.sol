fc6f9c1e7b5917e0dedab2eba60a9c7243724dda
Oleh Andrushko <olich.me@gmail.com>
2023-03-14 12:15:08 +0100

    chore: fix linting

--- contracts/Artiflyx.sol
+++ contracts/Artiflyx.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol" as ERC721Upgradeable;
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol" as ERC721URIStorageUpgradeable;
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol" as PausableUpgradeable;
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol" as AccessControlUpgradeable;
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol" as Initializable;
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol" as UUPSUpgradeable;
-import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol" as CountersUpgradeable;
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
 contract Artiflyx is
     Initializable,
